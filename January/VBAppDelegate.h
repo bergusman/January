ce294901feb0f943055ef9539e784f87ca62e120
Vitaly Berg <bergusman@gmail.com>
2014-01-30 16:09:59 +0400

    Initial Commit

---
+++ January/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  January
+//
+//  Created by Vitaly Berg on 30/01/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ January/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  January
+//
+//  Created by Vitaly Berg on 30/01/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
