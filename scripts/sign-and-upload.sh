2f44d79c98987bfeb395cd41b246b9b29792da7b
Vitaly Berg <bergusman@gmail.com>
2014-02-02 02:30:48 +0400

    Add Testflight

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -4,3 +4,14 @@ PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+curl http://testflightapp.com/api/builds.json \
+  -F file="@$OUTPUTDIR/$APPNAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APPNAME.app.dSYM.zip" \
+  -F api_token="$TESTFLIGHT_API_TOKEN" \
+  -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+  -F distribution_lists='Internal' \
+  -F notes="$RELEASE_NOTES"
