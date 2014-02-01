ec46136aab34c56d1e914e3c2c9922cb6c77d71d
Vitaly Berg <bergusman@gmail.com>
2014-02-02 02:48:14 +0400

    Fix some problems

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -3,14 +3,14 @@
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
 curl http://testflightapp.com/api/builds.json \
-  -F file="@$OUTPUTDIR/$APPNAME.ipa" \
-  -F dsym="@$OUTPUTDIR/$APPNAME.app.dSYM.zip" \
+  -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
   -F api_token="$TESTFLIGHT_API_TOKEN" \
   -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
   -F distribution_lists='Internal' \
