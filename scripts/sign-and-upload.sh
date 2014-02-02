1eb60dd354c1a4cfef6d0f78bdb46150c150055c
Vitaly Berg <bergusman@gmail.com>
2014-02-02 17:20:48 +0400

    Some changes in test flight comment

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -6,7 +6,7 @@ OUTPUTDIR="$PWD/build/Release-iphoneos"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
-RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE\nCommit: $TRAVIS_COMMIT"
 
 zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 
